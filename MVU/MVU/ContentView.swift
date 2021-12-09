aafe0043ff08fafc444095f8d7850f42c2f5cdf8
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-12-09 08:56:45 +0100

    StateObject

--- MVU/MVU/ContentView.swift
+++ MVU/MVU/ContentView.swift
@@ -1,28 +1,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject var viewModel : ContentViewModel
+    @StateObject var viewModel = ContentViewModel()
     
     var body: some View {
         VStack(alignment: .leading) {
             TextField("Enter forename", text: $viewModel.forename)
             TextField("Enter surname", text: $viewModel.surname)
-            HStack {
-                Text("Forename")
-                Text(viewModel.forename)
-            }
-            HStack {
-                Text("Surname")
-                Text(viewModel.surname)
-            }
+            NameView()
             Text(viewModel.greeting)
         }
         .padding(.horizontal)
+        .environmentObject(viewModel)
+    }
+}
+
+struct NameView: View {
+    @EnvironmentObject var viewModel: ContentViewModel
+    
+    var body: some View {
+        HStack {
+            Text("Forename")
+            Text(viewModel.forename)
+        }
+        HStack {
+            Text("Surname")
+            Text(viewModel.surname)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(viewModel: ContentViewModel())
+        ContentView()
     }
 }
