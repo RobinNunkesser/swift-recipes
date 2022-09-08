e1ddd854968ef76017c414188e68a9e92c2de924
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-09-08 17:13:50 +0200

    New storyboard recipe

---
+++ StoryboardRecipes/Navigation/Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Prof. Dr. Nunkesser, Robin on 08.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
